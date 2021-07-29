2b1239a53fa2cef8cc7c72612a900e8e2fd5a5c0
zhixiangyao <blueskyzx@icloud.com>
2021-07-29 18:38:08 +0800

    fix: type bug

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -4,5 +4,5 @@ import type { TypedUseSelectorHook } from 'react-redux'
 import type { AppDispatch, RootState } from '/@/store'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/store/index.ts
+++ src/store/index.ts
@@ -11,4 +11,4 @@ export default store
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = ReturnType<typeof store.dispatch>
+export type AppDispatch = typeof store.dispatch

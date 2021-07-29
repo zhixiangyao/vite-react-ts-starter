b4d30dff09c1dfb065bd99b9d6dca456ac5c7cc1
zhixiangyao <blueskyzx@icloud.com>
2021-07-29 17:40:40 +0800

    feat: use typescript-plugin-css-modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from '/@/assets/logo.svg'
+import style from '/@/App.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header bg-blue-800">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={style.App}>
+      <header className={`bg-blue-800 ${style.AppHeader}`}>
+        <img src={logo} className={style.AppLogo} alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" className={style.AppButton} onClick={() => setCount(count => count + 1)}>
             count is: {count}
           </button>
         </p>
@@ -19,17 +19,12 @@ function App() {
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className={style.AppLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
           {' | '}
           <a
-            className="App-link"
+            className={style.AppLink}
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"

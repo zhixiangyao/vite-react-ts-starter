312612d8be8a6297bcad3ff23676ececd086cd65
zhixiangyao <blueskyzx@icloud.com>
2021-07-30 11:54:12 +0800

    feat

--- src/store/counter/counterSlice.ts
+++ src/store/counter/counterSlice.ts
@@ -1,23 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { getLocalStorage, setLocalStorage } from '/@/utils'
+
+const initialState = {
+  value: getLocalStorage('value') ? Number(getLocalStorage('value')) : 0,
+}
 
 export const counterSlice = createSlice({
   name: 'counter',
-  initialState: {
-    value: 0,
-  },
+  initialState,
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1
+      setLocalStorage('value', state.value)
     },
     decrement: (state) => {
       state.value -= 1
+      setLocalStorage('value', state.value)
     },
     incrementByAmount: (state, action) => {
       state.value += action.payload
+      setLocalStorage('value', state.value)
     },
   },
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,31 @@
 const getElementById = (id: string) => document.getElementById(id)
 
-export { getElementById }
+const keyBefore = 'vite-react-ts-'
+
+const setLocalStorage = (key: string, value: any): void => {
+  value &&
+    localStorage.setItem(
+      `${keyBefore}${key}`,
+      typeof value === 'string' ? value : JSON.stringify(value),
+    )
+}
+
+const getLocalStorage = (key: string, isParse = false): any => {
+  const cache = localStorage.getItem(`${keyBefore}${key}`) ?? ''
+  return isParse ? JSON.parse(cache) : cache
+}
+
+const setSessionStorage = (key: string, value: any): void => {
+  value &&
+    sessionStorage.setItem(
+      `${keyBefore}${key}`,
+      typeof value === 'string' ? value : JSON.stringify(value),
+    )
+}
+
+const getSessionStorage = (key: string, isParse = false): any => {
+  const cache = sessionStorage.getItem(`${keyBefore}${key}`) ?? ''
+  return isParse ? JSON.parse(cache) : cache
+}
+
+export { getElementById, setLocalStorage, getLocalStorage, setSessionStorage, getSessionStorage }

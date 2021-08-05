f14c266ce5de8a77b4c2ea2e693bf0918ceaf22e
zhixiangyao <blueskyzx@icloud.com>
2021-08-05 18:57:24 +0800

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ import type { UserConfigExport } from 'vite'
 export default ({ command, mode }): UserConfigExport => {
   /**
    * Such as:
-   * import.meta.env.MODE: {string} app runtime 的模式。
+   * import.meta.env.MODE: {string}     app runtime 的模式。
    * import.meta.env.BASE_URL: {string} 部署 app 时的基本 URL 。他由 base 配置项决定。
-   * import.meta.env.PROD: {boolean} app 是否 runtime 在生产环境。
-   * import.meta.env.DEV: {boolean} app 是否 runtime 在开发环境 (永远与 import.meta.env.PROD 相反)。
+   * import.meta.env.PROD: {boolean}    app 是否 runtime 在生产环境。
+   * import.meta.env.DEV: {boolean}     app 是否 runtime 在开发环境 (永远与 import.meta.env.PROD 相反)。
    */
 
   const { VITE_APP_NODE_ENV, VITE_APP_TITLE } = getEnv(mode)

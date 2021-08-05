0e981dc57b6fd470600d7bf8768fccac3d76194a
zhixiangyao <blueskyzx@icloud.com>
2021-08-05 21:12:30 +0800

    chore: 持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,9 @@
 import { action, observable, makeObservable } from 'mobx'
+import { getLocalStorage, setLocalStorage } from '/@/utils'
 
 class Store {
   @observable
-  count = 1
+  count = getLocalStorage('value') ? Number(getLocalStorage('value')) : 0
 
   constructor() {
     makeObservable(this)
@@ -11,6 +12,7 @@ class Store {
   @action
   setCount = () => {
     this.count++
+    setLocalStorage('value', this.count)
   }
 }
 

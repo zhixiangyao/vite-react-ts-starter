891c5febc1bd7f6d820c735b34cea25a6a5df004
zhixiangyao <blueskyzx@icloud.com>
2021-09-15 10:59:56 +0800

    feat: add demo useReducer

---
+++ src/views/Test/component/Counter.tsx
@@ -0,0 +1,41 @@
+import React, { useReducer } from 'react'
+
+const initialState = { count: 0 }
+
+const reducer = (state: { count: number }, action: { type: 'decrement' | 'increment' }) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
+
+const Counter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <>
+      <div>Count: {state.count}</div>
+      <div className="flex w-52">
+        <button
+          className="w-1/2 flex items-center justify-center bg-lime-300 text-black border border-black shadow-offset-black"
+          onClick={() => dispatch({ type: 'decrement' })}
+        >
+          -
+        </button>
+
+        <button
+          className="w-1/2 flex items-center justify-center border border-black shadow-offset-black"
+          onClick={() => dispatch({ type: 'increment' })}
+        >
+          +
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Counter
--- src/views/Test/index.tsx
+++ src/views/Test/index.tsx
@@ -2,18 +2,25 @@ import React from 'react'
 
 import { ThemeContext, useTheme } from './context'
 import Content from './component/Content'
+import Counter from './component/Counter'
 
 const Test = () => {
   const value = useTheme()
 
   return (
-    <fieldset>
-      <legend>Context.Provider Test</legend>
+    <>
+      <Counter />
 
-      <ThemeContext.Provider value={value}>
-        <Content />
-      </ThemeContext.Provider>
-    </fieldset>
+      <br />
+
+      <fieldset>
+        <legend>Context.Provider Test</legend>
+
+        <ThemeContext.Provider value={value}>
+          <Content />
+        </ThemeContext.Provider>
+      </fieldset>
+    </>
   )
 }
 

c94679939d6767fdc21053ee75ff572e66ed6b05
zhixiangyao <blueskyzx@icloud.com>
2021-10-22 10:43:02 +0800

    chore

--- src/views/Test/component/MemoUseCallback.tsx
+++ src/views/Test/component/MemoUseCallback.tsx
@@ -8,7 +8,13 @@ interface ChildProps {
 const Child = (props: ChildProps) => {
   const { value, onChange } = props
   console.log('child fc running')
-  return <input value={value} placeholder="Child" onChange={(e) => onChange(e.target.value)} />
+
+  return (
+    <>
+      <h2>Child: {value}</h2>
+      <input value={value} placeholder="Child" onChange={(e) => onChange(e.target.value)} />
+    </>
+  )
 }
 
 const MemoChild = memo(Child)
@@ -17,18 +23,14 @@ const Compo = () => {
   const [a, setA] = useState('')
   const [b, setB] = useState('')
 
-  const onChangeMemoChild = useCallback(
-    (e) => {
-      setB(e)
-    },
-    [setB],
-  )
+  const onChangeMemoChild = useCallback((e) => setB(e), [setB])
 
   return (
     <fieldset className="w-240px">
       <legend>MemoUseCallback</legend>
 
       <div className="flex flex-col">
+        <h2>Compo: {a}</h2>
         <input placeholder="Compo" value={a} onChange={(e) => setA(e.target.value)} />
 
         <MemoChild value={b} onChange={onChangeMemoChild} />

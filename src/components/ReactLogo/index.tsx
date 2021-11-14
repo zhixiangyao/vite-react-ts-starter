c21b159f6a9422c1ca7dbe2be0026054ceebeb32
zhixiangyao <blueskyzx@icloud.com>
2021-11-14 17:21:13 +0800

    refactor: use styled-components

--- src/components/ReactLogo/index.tsx
+++ src/components/ReactLogo/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import logo from '/@/assets/logo.svg'
-import style from './index.module.css'
+import styled from 'styled-components'
+
+const Img = styled.img`
+  height: 40vmin;
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: react-logo-spin infinite 20s linear;
+  }
+
+  @keyframes react-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`
 
 const ReactLogo = () => (
   <>
-    <img src={logo} className={`pointer-events-none ${style.ReactLogo}`} alt="logo" />
+    <Img src={logo} className="pointer-events-none" alt="logo" />
     <p>Hello Vite + React + ReactRouter + ReactMobx + TypeScript + StyledComponents + WindiCss!</p>
   </>
 )

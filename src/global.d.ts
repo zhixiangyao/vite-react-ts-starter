5321d04c1a344a1bcfd54deeb9630d4aaab298fd
zhixiangyao <blueskyzx@icloud.com>
2024-09-19 11:36:20 +0800

    chore

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,15 +1,4 @@
 declare global {
-  /**
-   * https://cn.vitejs.dev/guide/env-and-mode.html#intellisense
-   *
-   * Intelligent hint:
-   * By default, Vite provides type definitions for import.meta.env.
-   * As more and more ENV-variables are customized in the .env[mode] file
-   * You may want to get TypeScript IntelliSense for these user-defined ENV-variables prefixed with Vite in your code.
-   * To do this, you can create an env.d.ts file in the src directory
-   * Then add the definition of ImportMetaEnv as follows:
-   */
-
   interface ImportMetaEnv {
     VITE_APP_NODE_ENV: string
     VITE_APP_TITLE: string

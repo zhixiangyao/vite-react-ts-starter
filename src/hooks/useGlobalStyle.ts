7daf6801ba19706ddab8021ee398418c4fa2171d
zhixiangyao <blueskyzx@icloud.com>
2024-09-19 11:36:10 +0800

    feat: add hook `useGlobalStyle`

---
+++ src/hooks/useGlobalStyle.ts
@@ -0,0 +1,17 @@
+import { useCallback, useEffect } from 'react'
+
+export const useGlobalStyle = (className?: string) => {
+  const remove = useCallback(() => {
+    className && document.body.classList.remove(className)
+  }, [className])
+
+  useEffect(() => {
+    className && document.body.classList.add(className)
+
+    return () => {
+      className && document.body.classList.remove(className)
+    }
+  }, [className])
+
+  return { remove }
+}

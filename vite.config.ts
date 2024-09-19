b72d1f295e429a12f99f6fe0f6095d5e150e1cdb
zhixiangyao <blueskyzx@icloud.com>
2024-09-19 11:35:34 +0800

    feat: add `vite-plugin-checker` to check for runtime type errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import React from '@vitejs/plugin-react'
 import type { ConfigEnv } from 'vite'
+import checker from 'vite-plugin-checker'
 
 /**
  * https://vitejs.dev/config/
  */
 const baseConfig = {
-  plugins: [React()],
+  plugins: [React(), checker({ typescript: true })],
   resolve: {
     alias: [
       {
@@ -21,41 +22,11 @@ const baseConfig = {
   },
 }
 
-interface ENV {
-  [K: string]: string
-}
-
-const getEnv = (mode: string) => {
-  const envFileName = `.env.${mode}`
-  const envObject = Object.create(null) as ENV
-
-  try {
-    const envConfig = dotenv.parse(fs.readFileSync(envFileName))
-    for (const k in envConfig) Object.assign(envObject, { [k]: envConfig[k] })
-    return envObject
-  } catch (error) {
-    console.error(error)
-    return envObject
-  }
-}
-
 export default ({ mode, command }: ConfigEnv) => {
-  /**
-   * Such as:
-   * import.meta.env.MODE: {string}     The mode of the app runtime.
-   * import.meta.env.BASE_URL: {string} The base URL for deploying the app. This is determined by the base configuration entry.
-   * import.meta.env.PROD: {boolean}    Whether the app is runtime in the production environment.
-   * import.meta.env.DEV: {boolean}     Whether app runtime is in the development environment (always the opposite of import.meta.env.PROD).
-   */
+  const { VITE_APP_NODE_ENV, VITE_APP_TITLE } = dotenv.parse(fs.readFileSync(`.env.${mode}`))
 
-  const { VITE_APP_NODE_ENV, VITE_APP_TITLE } = getEnv(mode)
-
-  setTimeout(() => {
-    console.log()
-    console.log('\x1b[33m%s\x1b[0m', `🏭--NODE ENV (VITE_APP_NODE_ENV): ${VITE_APP_NODE_ENV}`)
-    console.log('\x1b[36m%s\x1b[0m', `🏠--APP TITLE (VITE_APP_TITLE): ${VITE_APP_TITLE}`)
-    console.log()
-  }, 66)
+  console.log('\x1b[33m%s\x1b[0m', `🏭--NODE ENV (VITE_APP_NODE_ENV): ${VITE_APP_NODE_ENV}`)
+  console.log('\x1b[36m%s\x1b[0m', `🏠--APP TITLE (VITE_APP_TITLE): ${VITE_APP_TITLE}`)
 
   if (command === 'serve') {
     return defineConfig({ ...baseConfig })
